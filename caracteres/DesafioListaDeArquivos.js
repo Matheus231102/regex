fc2877a66de72ab65491e06f79c46b23d5fb3922
Matheus de Souza Badia <matheusbadia23@gmail.com>
2024-04-18 16:07:59 -0300

    capturar quantidade de arquivos numa lista de mp3, verificar quantidade de espaços

---
+++ caracteres/DesafioListaDeArquivos.js
@@ -0,0 +1,8 @@
+// quantas referencias de .mp3 estão contidas no texto ?
+const texto = 'list de arquivos mp3: jazz.mp3,rock.mp3,podcast.mp3,blues.mp3'
+const regexMP3 = /\.mp3/g
+
+const quantidadeMp3 = texto.match(regexMP3)
+console.log(mp3files)
+
+
